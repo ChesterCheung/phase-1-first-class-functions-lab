063337b2543277ac07072b065adc718368865fe7
Chester <cheung319@gmail.com>
2021-11-10 00:06:23 -0500

    commit tree 1c60405039b38fc8b6c2dd0552aecc4cb8c76b84

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solution in this file!
+const drivers = ['Sally', 'Bob', 'Freddy', 'Claudia'];
+const returnFirstTwoDrivers = function(){
+    return(drivers.slice(0 , 2));
+}
+const returnLastTwoDrivers = function(){
+    return(drivers.slice(2, 4));
+}
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFaremultiplier(number){
+    return number;
+}
+
+createFaremultiplier(function() {number * 4});
